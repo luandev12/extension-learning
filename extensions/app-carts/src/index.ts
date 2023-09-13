71b7a2d282e486b765741f0e096469f65b7bc9d7
Tran Luan <luanitdev@gmail.com>
2023-09-13 17:15:20 +0700

    v1.1 functions api

---
+++ extensions/app-carts/src/index.ts
@@ -0,0 +1,35 @@
+import { InputQuery, FunctionResult, FunctionError } from '../generated/api';
+
+interface Error {
+  localizedMessage: string;
+  target: string;
+}
+
+export default (input) => {
+  const error: Error = {
+    localizedMessage:
+      'There is an order maximum of $1,000 for customers without established order history',
+    target: 'cart'
+  };
+  const orderSubtotal = parseFloat(input.cart.cost.subtotalAmount.amount);
+  const errors: Error[] = [];
+
+  if (orderSubtotal > 1) {
+    if (input.cart.buyerIdentity && input.cart.buyerIdentity.customer) {
+      if (input.cart.buyerIdentity.customer.numberOfOrders < 5) {
+        errors.push(error);
+      }
+      // if VN not add to car
+      if (input.localization.country.isoCode == 'UK') {
+        errors.push({
+          localizedMessage: 'VN Not Add to Card',
+          target: 'cart'
+        });
+      }
+    } else {
+      errors.push(error);
+    }
+  }
+
+  return { errors };
+};
---
+++ extensions/app-upsell/graphql/query.tsx
@@ -0,0 +1,21 @@
+export const getProducts = `query ($first: Int!) {
+    products(first: $first) {
+      nodes {
+        id
+        title
+        images(first:1){
+          nodes {
+            url
+          }
+        }
+        variants(first: 1) {
+          nodes {
+            id
+            price {
+              amount
+            }
+          }
+        }
+      }
+    }
+  }`;
