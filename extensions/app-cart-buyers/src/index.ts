52a7199f63cf9b0d414686a864369bde5b1a3b0e
Tran Luan <luanitdev@gmail.com>
2023-09-14 14:48:56 +0700

    v1.4 order history

---
+++ extensions/app-cart-buyers/src/index.ts
@@ -0,0 +1,27 @@
+import { InputQuery, FunctionResult, FunctionError } from '../generated/api';
+
+interface Error {
+  localizedMessage: string;
+  target: string;
+}
+
+export default (input: InputQuery) => {
+  const error: Error = {
+    localizedMessage: `There is an order maximum of $10 for customers without established order history ${input.cart.buyerIdentity?.customer?.metafield?.value}`,
+    target: 'cart'
+  };
+  const orderSubtotal = parseFloat(input.cart.cost.subtotalAmount.amount);
+  const errors: Error[] = [];
+
+  if (orderSubtotal > 10) {
+    if (input.cart.buyerIdentity && input.cart.buyerIdentity?.customer) {
+      if (input.cart?.buyerIdentity?.customer?.numberOfOrders < 5) {
+        errors.push(error);
+      }
+    } else {
+      errors.push(error);
+    }
+  }
+
+  return { errors };
+};
